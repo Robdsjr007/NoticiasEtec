3210f95c0c2ba62bb357a3ea4e2483c0c5827ae2
Replit user <>
2023-05-31 11:29:48 -0300

    mudanças

---
+++ assets/js/changemode.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.ChangeMode')
+
+btn.addEventListener('click', function(e){
+    e.preventDefault
+    
+})
---
+++ noticia/assets/js/scroll.js
@@ -0,0 +1,16 @@
+  window.addEventListener('scroll', function() {
+    var scrollButton = document.querySelector('.scroll-top');
+    if (window.pageYOffset > 200) {
+      scrollButton.classList.add('show-scroll');
+    } else {
+      scrollButton.classList.remove('show-scroll');
+    }
+  });
+
+
+  
+
+
+ 
+
+
